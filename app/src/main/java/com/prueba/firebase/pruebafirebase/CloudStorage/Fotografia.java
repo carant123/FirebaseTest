ad78647517c07621ca9810883f98cd63d7da2dfe
carant123 <carant896@hotmail.com>
2018-12-12 23:56:00 -0500

    comentario uri imagen

--- app/src/main/java/com/prueba/firebase/pruebafirebase/CloudStorage/Fotografia.java
+++ app/src/main/java/com/prueba/firebase/pruebafirebase/CloudStorage/Fotografia.java
@@ -1,20 +1,27 @@
 package com.prueba.firebase.pruebafirebase.CloudStorage;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatImageView;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.storage.StorageReference;
 import com.prueba.firebase.pruebafirebase.R;
+
+import java.io.IOException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -56,10 +63,11 @@ public class Fotografia extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
+                case R.id.navigation_gallery:
                     mTextMessage.setText(R.string.main_label_galeria);
+                    fromGallery();
                     return true;
-                case R.id.navigation_dashboard:
+                case R.id.navigation_camera:
                     mTextMessage.setText(R.string.main_label_camara);
                     return true;
             }
@@ -76,6 +84,39 @@ public class Fotografia extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private void fromGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent,RC_GALLERY);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK){
+            switch(requestCode){
+                case RC_GALLERY:
+                    if(data != null){
+                        mPhotoSelectedUri = data.getData();
+
+                        try {
+                            Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),
+                                    mPhotoSelectedUri);
+                            imgPhoto.setImageBitmap(bitmap);
+                            btnDelete.setVisibility(View.GONE);
+                            mTextMessage.setText("¿desea subir esta imagen?");
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    break;
+                case RC_CAMERA:
+                    break;
+            }
+        }
     }
 
     @OnClick(R.id.btnUpload)

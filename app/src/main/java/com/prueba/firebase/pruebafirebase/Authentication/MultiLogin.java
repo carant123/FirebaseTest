650018f59d732c9267c99e2e634075ee2331e591
carant123 <carant896@hotmail.com>
2018-12-17 23:26:24 -0500

    comentario uri imagen 4

--- app/src/main/java/com/prueba/firebase/pruebafirebase/Authentication/MultiLogin.java
+++ app/src/main/java/com/prueba/firebase/pruebafirebase/Authentication/MultiLogin.java
@@ -1,16 +1,52 @@
 package com.prueba.firebase.pruebafirebase.Authentication;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.prueba.firebase.pruebafirebase.R;
 
 public class MultiLogin  extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.multilogin_layout);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+
+                }else{
+
+                }
+
+
+            }
+        };
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(mAuthStateListener != null){
+            mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+        }
     }
 }
